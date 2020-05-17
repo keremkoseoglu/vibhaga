b97f87dfa60d2a7dbd1aabbf107ff5248e349742
keremkoseoglu <kerem@keremkoseoglu.com>
2020-05-17 15:17:36 +0300

    Initial submission

---
+++ main.py
@@ -0,0 +1,12 @@
+# todo
+"""
+geliştirme
+    vibhaga klasörü aç
+    GTR içeriğini al
+    Joseki içeriğini al
+    Databus içeriğini al
+    test
+yayın 
+    databus'ten kopya çekip ilgili dosyaları ekle
+    readme.md ekle
+"""
